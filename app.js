b9984a3f620a4901d0987dd65a6fdc3800c64cd9
JigarSwam <jswaminarayan@gmail.com>
2025-02-20 22:08:54 -0800

    create basic math functions

---
+++ app.js
@@ -0,0 +1,17 @@
+let add = (a, b) => a + b;
+
+let subtract = (a, b) => a - b;
+
+let multiply = (a, b) => a * b;
+
+let divide = (a, b) => a / b;
+
+
+let valueA;
+let valueB;
+let operator;
+
+let operate = (a, b, op) => {
+    // switch statement for operators and call above functions based on said operator
+
+}
